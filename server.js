07398fa805f007edd90058f202225611223380b0
John Doe <johndoe@example.com>
2022-11-08 23:18:51 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 1113;
+
+//dotnev
+require('dotenv').config();
+//connect to mongo db
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch(err => console.log(err));
+
+//set ejs
+app.set('view engine', 'ejs');
+//url encoded
+app.use(express.urlencoded({ extended: true }));
+// expres static
+app.use('/static', express.static('static'))
+//app.use file upload
+//import routes
+const routes = require('./routes/routes.js');
+app.use('/', routes);
+app.listen(process.env.PORT || 5050, () => console.log(`Example app listening on port 5050!`));
